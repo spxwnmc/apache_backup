ca50ef2458d09f868b937f50b257f366aafc9f63
spawmc <spawnmcsqrt@gmail.com>
2021-09-12 00:33:31 -0500

    Se agregaron las funciones basicas para manipular salidas y controles de usuarios

--- apache-backup.sh
+++ apache-backup.sh
@@ -6,3 +6,25 @@ src_www="/var/www/"
 dest_apache="/apache2/"
 dest_www="/www/"
 
+function error_msg () {
+    echo -e "\e[0;31m\033[1m[!]\033[0m\e[0m ${1}"
+}
+
+function process_msg () {
+    echo -e "\e[0;34m\033[1m[+]\033[0m\e[0m ${1}"
+}
+
+function success_msg () {
+    echo -e "\e[0;32m\033[1m[+]\033[0m\e[0m ${1}"
+}
+
+function is_root() {
+    if ((${EUID:-0} || "$(id -u)")); then
+        error_msg "This script must be run as root"; exit 1;
+    fi
+}
+
+function create_folders () {
+    mkdir -p ${backup_path}/{${dest_apache},${dest_www}} &&\
+        success_msg "Folders created successfully"
+}

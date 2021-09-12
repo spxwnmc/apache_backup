27cef58062fdd8aefb92924bb867232c5471b01d
spawmc <spawnmcsqrt@gmail.com>
2021-09-12 12:20:09 -0500

    Se corrigieron problemas con algunas rutas y se agrego funcion para comprimir los archivos

--- apache-backup.sh
+++ apache-backup.sh
@@ -2,9 +2,8 @@
 
 backup_path="/tmp/backups_httpd/backup$(date +%F-%H-%M)"
 src_apache="/etc/apache2/"
-src_www="/var/www/"
-dest_apache="/apache2/"
-dest_www="/www/"
+src_www="/var/www"
+compress_path="${backup_path}.tar.gz"
 days_to_keep=2
 
 function error_msg () {
@@ -19,6 +18,10 @@ function success_msg () {
     echo -e "\e[0;32m\033[1m[+]\033[0m\e[0m ${1}"
 }
 
+function help () {
+    echo -e "\nUsage: $(basename ${backup_path})"
+}
+
 function is_root() {
     if ((${EUID:-0} || "$(id -u)")); then
         error_msg "This script must be run as root"; exit 1;
@@ -26,7 +29,7 @@ function is_root() {
 }
 
 function create_folders () {
-    mkdir -p ${backup_path}/{${dest_apache},${dest_www}} &&\
+    mkdir -p ${backup_path} &&\
         success_msg "Folders created successfully"
 }
 
@@ -34,5 +37,12 @@ function delete_old_backups () {
     if [ ${days_to_keep} -gt 0 ]; then
         process_msg "Deleting backups older then ${days_to_keep} days"
         find ${backup_path}/* -mtime +${days_to_keep} -exec rm {} \;
+        success_msg "Folders deleted successfully"
     fi
+}
+
+function compress_file () {
+    tar -cpzf "${compress_path}"\
+        ${src_apache}\
+        ${src_www} &>/dev/null
 }
